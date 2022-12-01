16aa3764b21c5ed5b8591470e891dbce8528a074
Dylan Hall <dhall1@mail.csuchico.edu>
2022-11-30 17:07:01 -0800

    Added functionality to constructor(), entry(), length_difference(), and accuracy(). Wrote 15 tests for error checking.

--- tappity.cpp
+++ tappity.cpp
@@ -4,37 +4,55 @@
 **/
 
 //Constructor sets the reference phrase
-tappity::tappity(std::string reference)
-{
+tappity::tappity(std::string reference){
+  phrase = reference;
+  userinput = "";
 }
 
-//Provide the input to be compared to the reference. Before this 
+//Provide the input to be compared to the reference. Before this
 //function is called, the input should be considered an empty string
-void tappity::entry(std::string input)
-{
+void tappity::entry(std::string input){
+  userinput = input;
 }
 
 //Compares the length of the reference to that of the input and
 //returns the absolute value of the difference, or 0 if they are the
 //same length
-int tappity::length_difference()
-{
-  return 0;
+int tappity::length_difference(){
+  if(phrase.size() > userinput.size()){
+    return phrase.size() - userinput.size();
+  }
+  return userinput.size() - phrase.size();
 }
 
 //Compares the content of the reference to that of the input and
 //calculates the accuracy of matching characters. If the two strings
-//are identical, accuracy should be 100. However, for each 
+//are identical, accuracy should be 100. However, for each
 //corresponding character that does not match in the same location,
 //the percentage of corresponding characters should be returned.
 //For example, if the reference and input have 10 letters and the input
-//matches 8 of the corresponding reference characters, the function 
+//matches 8 of the corresponding reference characters, the function
 //should return 80. When input does not have the same number of
 //characters as the reference, the accuracy should represent the percent
 //of matching characters between the shorter and longer strings. For
 //example, if one string has 8 characters matching the corresponding
 //locations in another string that has 16 characters, the accuracy is 50.
-double tappity::accuracy()
-{
-  return 0;
+double tappity::accuracy(){
+  double correct = 0;
+  double small = 0;
+  double large = 0;
+
+  if(phrase.size() > userinput.size()){
+    small = userinput.size();
+    large = phrase.size();
+  }else{
+    large = userinput.size();
+    small = phrase.size();
+  }
+  for(int i = 0; i < small; i++){
+    if(phrase[i]==userinput[i]){
+      correct++;
+    }
+  }
+  return correct/large*100;
 }
--- tappityTest.cpp
+++ tappityTest.cpp
@@ -4,12 +4,116 @@
 
 #include <gtest/gtest.h>
 #include "tappity.h"
- 
+
 class tappityTest : public ::testing::Test
 {
 	protected:
-		tappityTest(){} 
-		virtual ~tappityTest(){} 
+		tappityTest(){}
+		virtual ~tappityTest(){}
 		virtual void SetUp(){}
 		virtual void TearDown(){}
 };
+
+TEST(tipityTest, LengthDifferenceNoInputOrReference){
+  tappity test = tappity("");
+  int result = test.length_difference();
+  ASSERT_EQ(result, 0);
+}
+
+TEST(tipityTest, LengthDifferenceNoInput){
+  tappity test = tappity("Love");
+  int result = test.length_difference();
+  ASSERT_EQ(result, 4);
+}
+
+TEST(tipityTest, LengthDifferenceLongConstructor){
+  tappity test = tappity("Death");
+  test.entry("love");
+  int result = test.length_difference();
+  ASSERT_EQ(result, 1);
+}
+
+TEST(tipityTest, LengthDifferenceLongInput){
+  tappity test = tappity("Love");
+  test.entry("Death");
+  int result = test.length_difference();
+  ASSERT_EQ(result, 1);
+}
+
+TEST(tipityTest, LengthDifferenceSame){
+  tappity test = tappity("Love");
+  test.entry("Love");
+  int result = test.length_difference();
+  ASSERT_EQ(result, 0);
+}
+
+TEST(tipityTest, AccuracyNoReference){
+  tappity test = tappity("");
+  test.entry("Death");
+  double result = test.accuracy();
+  ASSERT_EQ(result, 0);
+}
+
+TEST(tipityTest, AccuracyNoInput){
+  tappity test = tappity("Love");
+  test.entry("");
+  double result = test.accuracy();
+  ASSERT_EQ(result, 0);
+}
+
+TEST(tipityTest, AccuracyOffset){
+  tappity test = tappity("The worst is on it's way");
+  test.entry(" The worst is on it's way");
+  double result = test.accuracy();
+  ASSERT_EQ(result, 0);
+}
+TEST(tipityTest, AccuracyCorrect){
+  tappity test = tappity("The worst is on it's way");
+  test.entry("The worst is on it's way");
+  double result = test.accuracy();
+  ASSERT_EQ(result, 100);
+}
+
+TEST(tipityTest, AccuracyCorrectSymbols){
+  tappity test = tappity("*^&%)()@(><~`=-+_{}'""\"[]''><'`)");
+  test.entry("*^&%)()@(><~`=-+_{}'""\"[]''><'`)");
+  double result = test.accuracy();
+  ASSERT_EQ(result, 100);
+}
+
+TEST(tipityTest, AccuracyCorrectSpaces){
+  tappity test = tappity("                ");
+  test.entry("                ");
+  double result = test.accuracy();
+  ASSERT_EQ(result, 100);
+}
+
+TEST(tipityTest, AccuracyLongReference){
+  tappity test = tappity("This is a test!");
+  test.entry("This is a");
+  double result = test.accuracy();
+  ASSERT_EQ(result, 60);
+}
+
+TEST(tipityTest, AccuracyLongInput){
+  tappity test = tappity("This is a");
+  test.entry("This is a test!");
+  int result = test.accuracy();
+  ASSERT_EQ(result, 60);
+}
+
+TEST(tipityTest, AccuracyDecimals){
+  tappity test = tappity("Thi");
+  test.entry("This is ");
+  double result = test.accuracy();
+  ASSERT_EQ(result, 37.5);
+}
+
+TEST(tipityTest, AccuracyDecimalsRepeating){
+  tappity test = tappity("T");
+  test.entry("Thi");
+  double result = test.accuracy();
+  double expected = 1;
+  expected = expected/3*100;
+  ASSERT_EQ(result, expected);
+}
